bcdb68dc95a53f8e0db1c62e6e19744b96b9b2ea
Micheal Owen <mowen8983@gmail.com>
2024-05-24 13:18:07 +0700

    feat:adding mantine provider component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./App.module.css";
+import { MantineProvider } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
